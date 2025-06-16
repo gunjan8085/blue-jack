50aad03741833400ccb9d8004abeefcd5a66d2de
mayanksahu17 <mayank0real0world@gmail.com>
2025-06-16 17:59:10 +0530

    feat: update API_URL to production endpoint and add client directive in AddCategoryModal

--- backend.zifypay/app.js
+++ backend.zifypay/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
       console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl} - ${res.statusCode} (${duration}ms)`);
   });
 
-    next();
+next();
 });
 // Router
 app.use(router);
