8673233b58b577f7ade56bba2f9cf969e917ddbc
Yashvardhan Sharma <yashvardhansharma111@gmail.com>
2025-07-21 23:37:15 +0530

    here

--- frontend.zifypay/lib/const.ts
+++ frontend.zifypay/lib/const.ts
@@ -1,2 +1,2 @@
-// export const API_URL = "http://localhost:5001/api/v1";
-export const API_URL = "https://api.zifypay.com/api/v1"; 
+ export const API_URL = "http://localhost:5001/api/v1";
+//export const API_URL = "https://api.zifypay.com/api/v1"; 
