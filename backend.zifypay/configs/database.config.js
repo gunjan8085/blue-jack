7e06fbc05018dd475fcb607b63140bcb5b60aa1e
mayanksahu17 <mayank0real0world@gmail.com>
2025-07-21 22:06:02 +0530

    Update MongoDB URI in database.config.js to use new connection string

--- backend.zifypay/configs/database.config.js
+++ backend.zifypay/configs/database.config.js
@@ -10,8 +10,8 @@ const MONGODB_DB_NAME = process.env.MONGODB_DB_NAME;
 const MONGODB_PROJECT_NAME = process.env.MONGODB_PROJECT_NAME;
 
 // const MONGODB_URI = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_CLUSTER}.mongodb.net/${MONGODB_DB_NAME}?retryWrites=true&w=majority&appName=${MONGODB_PROJECT_NAME}`;
-const MONGODB_URI = `mongodb+srv://birlapranjal460:OSoLJzGoDlhPh5OT@ninexfoldmain.hb8fltn.mongodb.net/pos`;
-
+// const MONGODB_URI = `mongodb+srv://birlapranjal460:OSoLJzGoDlhPh5OT@ninexfoldmain.hb8fltn.mongodb.net/pos`;
+const MONGODB_URI = "mongodb+srv://mayank:KbxX46MFAUJ94YG0@zifypay.szgzcfw.mongodb.net/"
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(MONGODB_URI);
