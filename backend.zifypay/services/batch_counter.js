62028d334696d90e11073705689662d7a1441e20
mayanksahu17 <mayank0real0world@gmail.com>
2025-07-21 16:43:54 +0530

    Refactor import statements to use CommonJS syntax in batch_counter.js

--- backend.zifypay/services/batch_counter.js
+++ backend.zifypay/services/batch_counter.js
@@ -1,5 +1,6 @@
-import fs from 'fs';
-import path from 'path';
+
+const fs = require('fs');
+const path = require('path');
 
 export default function generateBatchId() {
   const currentDate = new Date();
