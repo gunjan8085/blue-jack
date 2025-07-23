262b40c61ca36b65d59c77b8506a7fd1d39b0efc
mayanksahu17 <mayank0real0world@gmail.com>
2025-07-23 21:28:52 +0530

    Refactor MongoDB connection logic to use environment-specific URIs and remove hardcoded credentials

--- backend.zifypay/configs/database.config.js
+++ backend.zifypay/configs/database.config.js
@@ -3,18 +3,18 @@ const mongoose = require("mongoose");
 
 // ############### MongoDB configs ###############
 
-const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
-const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
-const MONGODB_CLUSTER = process.env.MONGODB_CLUSTER;
-const MONGODB_DB_NAME = process.env.MONGODB_DB_NAME;
-const MONGODB_PROJECT_NAME = process.env.MONGODB_PROJECT_NAME;
-
-// const MONGODB_URI = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_CLUSTER}.mongodb.net/${MONGODB_DB_NAME}?retryWrites=true&w=majority&appName=${MONGODB_PROJECT_NAME}`;
-// const MONGODB_URI = `mongodb+srv://birlapranjal460:OSoLJzGoDlhPh5OT@ninexfoldmain.hb8fltn.mongodb.net/pos`;
-const MONGODB_URI = "mongodb+srv://mayank:KbxX46MFAUJ94YG0@zifypay.szgzcfw.mongodb.net/"
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+
+    console.log( "NODE_ENV " , process.env.NODE_ENV)
+    if (process.env.NODE_ENV === "development") {
+      console.log("Connecting to MongoDB in development mode...");
+          await mongoose.connect(process.env.MONGODB_URI_DEV);
+
+    } else if (process.env.NODE_ENV === "production") {
+      console.log("Connecting to MongoDB in production mode...");
+          await mongoose.connect(process.env.MONGODB_URI_PROD);
+    }
     console.log("Connected to MongoDB!");
   } catch (error) {
     console.error("MongoDB connection error:", error);
@@ -25,10 +25,3 @@ connectToDatabase();
 
 module.exports = connectToDatabase;
 
-
-// mayanksahu0024
-
-// 0XCvvsVMBfUHTqPK
-
-
-// mongodb+srv://mayanksahu0024:0XCvvsVMBfUHTqPK@zifypay.71cgbqb.mongodb.net/
