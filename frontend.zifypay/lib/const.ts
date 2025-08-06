e6dda20df6325bbb921fafe94799f63a4d920740
Anish-1144 <dev.anishmourya27@gmail.com>
2025-08-06 13:51:19 +0530

    Refactor API_URL to use environment variables for production and development configurations

--- frontend.zifypay/lib/const.ts
+++ frontend.zifypay/lib/const.ts
@@ -1,3 +1,24 @@
+// lib/const.ts
+const {
+  NODE_ENV,
+  NEXT_PUBLIC_DEVELOPMENT_URL: DEV_URL,
+  NEXT_PUBLIC_PRODUCTION_URL: PROD_URL,
+} = process.env;
 
-// export const API_URL = "http://localhost:5001/api/v1";
-export const API_URL = process.env.PRODUCTION_URL 
+// Provide fallbacks for development
+const developmentUrl = DEV_URL 
+const productionUrl = PROD_URL 
+
+export const API_URL =
+  NODE_ENV === "production" ? productionUrl : developmentUrl;
+
+
+
+
+
+//   // lib/const.ts
+// console.log('Environment variables:', {
+//   NODE_ENV: process.env.NODE_ENV,
+//   DEV_URL: process.env.NEXT_PUBLIC_DEVELOPMENT_URL,
+//   PROD_URL: process.env.NEXT_PUBLIC_PRODUCTION_URL,
+// });
